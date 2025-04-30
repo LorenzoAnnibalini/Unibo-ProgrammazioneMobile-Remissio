89fc4e38888344452d188e397c9347909e5d268b
Lorenzo Annibalini <116550591+LorenzoAnnibalini@users.noreply.github.com>
2025-04-30 10:15:46 +0200

    Add Xcode Project

---
+++ Remissio/Remissio/RemissioApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemissioApp.swift
+//  Remissio
+//
+//  Created by Lorenzo Annibalini on 30/04/25.
+//
+
+import SwiftUI
+
+@main
+struct RemissioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
