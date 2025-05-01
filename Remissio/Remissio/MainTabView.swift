e945bbe643a676ffdc72e7600318f70986447fda
Lorenzo Annibalini <116550591+LorenzoAnnibalini@users.noreply.github.com>
2025-05-01 10:48:33 +0200

    fixed environmentObject

--- Remissio/Remissio/MainTabView.swift
+++ Remissio/Remissio/MainTabView.swift
@@ -45,5 +45,6 @@ struct MainTabView: View {
 struct MainTabView_Previews: PreviewProvider {
     static var previews: some View {
         MainTabView()
+        .environmentObject(AuthenticationManager())
     }
 }
